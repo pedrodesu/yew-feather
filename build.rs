81817be40200b8084de8b694d7d0f4859f1711d7
Martin Molzer <ga65guy@mytum.de>
2021-11-05 02:57:21 +0100

    add build.rs for automated builds

---
+++ build.rs
@@ -0,0 +1,28 @@
+use std::io::{Error, ErrorKind, Result};
+use std::process::Command;
+
+fn main() -> Result<()>
+{
+    // Tell Cargo that if the given file changes, to rerun this build script.
+    println!("cargo:rerun-if-changed=bin/");
+    println!("cargo:rerun-if-changed=package-lock.json");
+    Command::new("npm")
+        .args(["ci", "--ignore-scripts", "--audit=false"])
+        .status()?
+        .success()
+        .then(|| ())
+        .ok_or(Error::new(
+            ErrorKind::Other,
+            "failed to install npm dependencies",
+        ))?;
+    Command::new("npm")
+        .args(["run-script", "build"])
+        .status()?
+        .success()
+        .then(|| ())
+        .ok_or(Error::new(
+            ErrorKind::Other,
+            "failed to install npm dependencies",
+        ))?;
+    Ok(())
+}
