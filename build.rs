769d3a1b2165d75f251ed30e1b71c40b6f980f33
Bruno Rigo Werminghoff <bwerminghoff@gmail.com>
2023-11-09 20:28:28 +0000

    Disable build script

--- build.rs
+++ build.rs
@@ -1,57 +1,57 @@
-use convert_case::{Case, Casing};
+// use convert_case::{Case, Casing};
 use std::{
-    collections::HashMap,
-    env, fs,
+//     collections::HashMap,
+//     env, fs,
     io::{BufReader, Result},
-    path::PathBuf,
-    process::Command,
+//     path::PathBuf,
+//     process::Command,
 };
-use which::which;
+// use which::which;
 
-const LIB_SLUG: &str = include_str!("build/lib_slug.rs");
-const SLUG: &str = include_str!("build/slug.rs");
+// const LIB_SLUG: &str = include_str!("build/lib_slug.rs");
+// const SLUG: &str = include_str!("build/slug.rs");
 
-const PACKAGE_MANAGERS: &[&str] = &["pnpm", "yarn", "npm"];
+// const PACKAGE_MANAGERS: &[&str] = &["pnpm", "yarn", "npm"];
 
-#[inline]
-fn get_package_manager() -> PathBuf {
-    PACKAGE_MANAGERS
-        .into_iter()
-        .find_map(|&m| which(m).ok())
-        .unwrap()
-}
+// #[inline]
+// fn get_package_manager() -> PathBuf {
+//     PACKAGE_MANAGERS
+//         .into_iter()
+//         .find_map(|&m| which(m).ok())
+//         .unwrap()
+// }
 
 fn main() -> Result<()> {
-    Command::new(get_package_manager())
-        .arg("install")
-        .status()?;
+    // Command::new(get_package_manager())
+    //     .arg("install")
+    //     .status()?;
 
-    let file =
-        fs::File::open(env::current_dir()?.join("node_modules/feather-icons/dist/icons.json"))?;
+    // let file =
+    //     fs::File::open(env::current_dir()?.join("node_modules/feather-icons/dist/icons.json"))?;
 
-    let collection: HashMap<String, String> = serde_json::from_reader(BufReader::new(file))?;
+    // let collection: HashMap<String, String> = serde_json::from_reader(BufReader::new(file))?;
 
-    let lib_content = [
-        LIB_SLUG.to_owned(),
-        collection
-            .into_iter()
-            .map(|(name, markup)| {
-                let markup = SLUG
-                    .replace("__ComponentName", name.to_case(Case::Pascal).as_str())
-                    .replace(
-                        "__component_func",
-                        format!("r#{}", name.to_case(Case::Snake)).as_str(),
-                    )
-                    .replace("__component_markup", markup.as_str());
+    // let lib_content = [
+    //     LIB_SLUG.to_owned(),
+    //     collection
+    //         .into_iter()
+    //         .map(|(name, markup)| {
+    //             let markup = SLUG
+    //                 .replace("__ComponentName", name.to_case(Case::Pascal).as_str())
+    //                 .replace(
+    //                     "__component_func",
+    //                     format!("r#{}", name.to_case(Case::Snake)).as_str(),
+    //                 )
+    //                 .replace("__component_markup", markup.as_str());
 
-                format!("\n{}\n", markup)
-            })
-            .collect(),
-    ]
-    .concat();
+    //             format!("\n{}\n", markup)
+    //         })
+    //         .collect(),
+    // ]
+    // .concat();
 
-    fs::create_dir_all("src").expect("could not generate src/lib.rs");
-    fs::write("src/lib.rs", lib_content.as_bytes()).expect("could not generate src/lib.rs");
+    // fs::create_dir_all("src").expect("could not generate src/lib.rs");
+    // fs::write("src/lib.rs", lib_content.as_bytes()).expect("could not generate src/lib.rs");
 
     Ok(())
 }
